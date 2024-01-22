77f7c710172f4c848d52e7211d1d6c6335a95576
Alan Reis <140574199+AlanBReis@users.noreply.github.com>
2024-01-22 01:09:13 -0300

    add api pokedex

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-console.log('Sucess!');
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error));
