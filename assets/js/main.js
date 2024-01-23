4ca8711d12a9c438b5f43e9d2d262f3066b2cf29
Alan Reis <140574199+AlanBReis@users.noreply.github.com>
2024-01-23 02:27:37 -0300

    finalizando dia 23

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-function convertPokemonToHtml(pokemon) {
+
+function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
         <span class="number">#001</span>
@@ -25,13 +23,6 @@ function convertPokemonToHtml(pokemon) {
 
 const pokemonList = document.getElementById("pokemonList");
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToHtml(pokemon);
-        }
-    })
-    .catch((error) => console.error(error));
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+});
