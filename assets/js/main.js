9eaac38fefcb5bd7f3cb47de87bf74c0866ad002
Alan Reis <140574199+AlanBReis@users.noreply.github.com>
2024-01-26 02:24:06 -0300

    convertendo pokeapi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,15 @@
-
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${pokemon.types.map((type) => `<li class="type"> ${type}</li>`).join('')}
             </ol>
             <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                src="${pokemon.photo}"
                 alt="${pokemon.name}"
             />
         </div>
